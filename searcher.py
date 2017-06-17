3230b44a4d422acfadcbedd2a910fbc181e9bab0
Jason Fiacco <jasonfiacco@yahoo.com>
2017-06-17 16:37:35 -0400

    Fixed language_button bugs and move through video feature

--- searcher.py
+++ searcher.py
@@ -1,13 +1,15 @@
+import sys
 import requests
+from lxml import html
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-from lxml import html
-from bs4 import BeautifulSoup
 import time
+
+WAIT_TIME = 10
 
 def extract_transcript(html_page):
     caption_tags = html_page.find_all('div', class_='caption-line-text')
@@ -17,68 +19,76 @@ def extract_transcript(html_page):
     caption_time_dictionary = dict(zip(caption_texts, time_texts))
     return caption_time_dictionary
 
-def get_caption_times(caption_time_dictionary, caption):
+def get_caption_times(caption_time_dictionary, target):
     times = []
     for key in caption_time_dictionary:
-        if caption in key:
+        if target in key:
             times.append(caption_time_dictionary[key])
     return times
 
 
 if __name__ == '__main__':
-    #url = input("Enter url of page: ")
+    url = input("Enter url of page: ")
 
     word = input("Enter the word you're searching for: ")
-    url = 'https://www.youtube.com/watch?v=4VwElW7SbLA&t=282s'
+    #url = 'https://www.youtube.com/watch?v=4VwElW7SbLA&t=282s'
     browser = webdriver.Chrome('/Users/chromedriver')
-    browser.get(url)
-    time.sleep(2)
+    try:
+        browser.get(url)
+    except:
+        print("Fatal error: Invalid URL")
+        sys.exit(0)
 
-    more_button = WebDriverWait(browser, 6).until(
-        EC.presence_of_element_located((By.ID, 'action-panel-overflow-button'))
+    time.sleep(1)
+
+    more_button = WebDriverWait(browser, WAIT_TIME).until(
+        EC.element_to_be_clickable((By.ID, 'action-panel-overflow-button'))
     )
     actions = ActionChains(browser)
     actions.move_to_element(more_button)
-    WebDriverWait(browser, 6).until(
+    WebDriverWait(browser, WAIT_TIME).until(
         EC.element_to_be_clickable((By.ID, 'action-panel-overflow-button'))
     )
     actions.click(more_button)
     actions.perform()
 
-    transcript_button = WebDriverWait(browser, 6).until(
+    transcript_button = WebDriverWait(browser, WAIT_TIME).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="action-panel-overflow-menu"]/li[2]/button'))
     )
     actions = ActionChains(browser)
     actions.move_to_element(transcript_button)
-    WebDriverWait(browser, 6).until(
+    WebDriverWait(browser, WAIT_TIME).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="action-panel-overflow-menu"]/li[2]/button'))
     )
     actions.click(transcript_button)
     actions.perform()
 
-    english_button = WebDriverWait(browser, 6).until(
+    english_button = WebDriverWait(browser, WAIT_TIME).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="watch-transcript-container"]/div[2]/div[1]/button'))
     )
     actions = ActionChains(browser)
     actions.move_to_element(english_button)
-    WebDriverWait(browser, 6).until(
+    WebDriverWait(browser, WAIT_TIME).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="watch-transcript-container"]/div[2]/div[1]/button'))
     )
     actions.click(english_button)
     actions.perform()
 
-    english_button2 = WebDriverWait(browser, 6).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="aria-menu-id-7"]/button'))
+    language_button = WebDriverWait(browser, WAIT_TIME).until(
+        EC.presence_of_element_located((By.XPATH, '//button[@class="yt-ui-menu-item yt-uix-menu-close-on-select yt-uix-button-menu-item" and ./span[contains(., "English")]]'))
     )
     actions = ActionChains(browser)
-    actions.move_to_element(english_button2)
-    WebDriverWait(browser, 6).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="aria-menu-id-7"]/button'))
+    actions.move_to_element(language_button)
+    WebDriverWait(browser, WAIT_TIME).until(
+        EC.element_to_be_clickable((By.XPATH, '//button[@class="yt-ui-menu-item yt-uix-menu-close-on-select yt-uix-button-menu-item" and ./span[contains(., "English")]]'))
     )
-    actions.click(english_button2)
+    actions.click(language_button)
     actions.perform()
 
-    WebDriverWait(browser, 6).until(
+    WebDriverWait(browser, WAIT_TIME).until(
+        EC.text_to_be_present_in_element((By.XPATH, '//*[@id="watch-transcript-container"]/div[2]/div[1]/button/span'), 'English')
+    )
+    WebDriverWait(browser, WAIT_TIME).until(
         EC.visibility_of_element_located((By.CLASS_NAME, 'caption-line-text'))
     )
 
@@ -86,4 +96,7 @@ if __name__ == '__main__':
     soup.prettify()
     caption_time_dictionary = extract_transcript(soup)
     times = get_caption_times(caption_time_dictionary, word)
-    print(times)
+    seconds = (times[0])[-2:]
+    minutes = (times[0])[:-3]
+    new_url = url + '&t={:s}m{:s}s'.format(minutes, seconds)
+    browser.get(new_url)
